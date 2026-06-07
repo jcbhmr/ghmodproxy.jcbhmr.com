f3d0a50990a1c00ea3a19c76e820cb54c0f2a03a
Jacob Hummer <jcbhmr@outlook.com>
2026-06-07 21:40:28 +0000

    save

---
+++ build.ts
@@ -0,0 +1,14 @@
+#!/usr/bin/env -S deno --allow-all
+import { execa } from "npm:execa@9.6.1"
+import * as path from "node:path"
+
+await execa({
+    verbose: "short",
+    stdio: "inherit",
+    env: {
+        GOLANG_VERSION: undefined,
+        GOROOT: undefined,
+        GOTOOLCHAIN: undefined,
+        GOPATH: undefined,
+    },
+})`./go/bin/go build .`
---
+++ install.ts
@@ -0,0 +1,20 @@
+#!/usr/bin/env -S deno --allow-all
+import { execa } from "npm:execa@9.6.1"
+import * as streamPromises from "node:stream/promises"
+import * as fs from "node:fs"
+import * as fsPromises from "node:fs/promises"
+
+const response = await fetch("https://go.dev/dl/go1.26.4.linux-amd64.tar.gz")
+if (response.status !== 200) {
+    throw new DOMException(`${response.url} ${response.status} ${response.statusText}`)
+}
+if (!response.body) {
+    throw new DOMException(`${response.url} no body`)
+}
+await streamPromises.pipeline(
+    response.body,
+    fs.createWriteStream("go1.26.4.linux-amd64.tar.gz")
+)
+await fsPromises.rm("go", { recursive: true, force: true })
+await execa({ verbose: "short", stdio: "inherit" })`tar -xzf go1.26.4.linux-amd64.tar.gz`
+await fsPromises.rm("go1.26.4.linux-amd64.tar.gz")
