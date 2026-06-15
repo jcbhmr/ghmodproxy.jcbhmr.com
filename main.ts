a1c06013e48ef6f501bf51f1ce55528376cd694f
Jacob Hummer <jcbhmr@outlook.com>
2026-06-15 07:08:36 +0000

    save

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env -S deno --allow-all
 import { DelimiterStream, TextLineStream } from "@std/streams"
 
-console.log("%cDENO_SERVE_ADDRESS=%o", "color: blue", Deno.env.get("DENO_SERVE_ADDRESS"))
+console.log("%cDENO_SERVE_ADDRESS=%o", "color: green", Deno.env.get("DENO_SERVE_ADDRESS"))
 const child = new Deno.Command(new URL(import.meta.resolve("./app")), {
     args: [],
     // env: {
@@ -12,20 +12,11 @@ const child = new Deno.Command(new URL(import.meta.resolve("./app")), {
     stderr: "inherit",
 }).spawn()
 child.unref()
-const [a, b] = child.stdout.tee()
 let firstLineBytes: Uint8Array | undefined
-for await (const line of a.pipeThrough(new DelimiterStream(Uint8Array.from("\n", c => c.codePointAt(0)!), { disposition: "suffix" }))) {
+for await (const line of child.stdout.pipeThrough(new DelimiterStream(Uint8Array.from("\n", c => c.codePointAt(0)!), { disposition: "suffix" }))) {
     firstLineBytes = line
     break;
 }
-void b.pipeTo(new WritableStream({
-    async write(chunk, _controller) {
-        await Deno.stdout.write(chunk)
-    },
-    async abort(reason) {
-        await Deno.stdout.writable.abort(reason)
-    },
-}))
 const firstLineText = firstLineBytes ? new TextDecoder().decode(firstLineBytes) : ""
 const listeningOn = firstLineText.match(/(http:\/\/\S+)/)?.[0]
 console.log("Listening on %c%s", "color: yellow", listeningOn ?? "<unknown>")
