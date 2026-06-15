4f69fe29a3dd164e6b1d403ed70113f20774ac93
Jacob Hummer <jcbhmr@outlook.com>
2026-06-15 07:44:53 +0000

    save

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"log/slog"
 	"net"
 	"net/http"
 	"os"
@@ -13,45 +14,15 @@ import (
 
 func init() {
 	http.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("Hello stdout!\n")
+		log.Printf("Hello log!")
+		slog.Info("GET /", "r.Method", r.Method, "r.URL", r.URL, "r.Header", r.Header)
 		fmt.Fprintf(w, "Hello, %s!\n", "Alan Turing")
 	})
 }
 
 func main() {
-	// port := uint16(8080)
-	// if v, ok := os.LookupEnv("PORT"); ok {
-	// 	port64, err := strconv.ParseUint(v, 10, 16)
-	// 	if err != nil {
-	// 		log.Fatal(err)
-	// 	}
-	// 	port = uint16(port64)
-	// }
-
-	// l, err := net.Listen("tcp", ":"+strconv.FormatUint(uint64(port), 10))
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// addr := l.Addr().(*net.TCPAddr)
-	// fmt.Printf("%d\n", addr.Port)
-
-	// err = http.Serve(l, nil)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
 	g, _ := errgroup.WithContext(context.TODO())
-	// g.Go(func() error {
-	// 	var l net.Listener
-	// 	l, err := vsock.ListenContextID(math.MaxUint32, 8080, nil)
-	// 	if err != nil {
-	// 		return err
-	// 	}
-
-	// 	// addr := l.Addr().(*vsock.Addr)
-	// 	// fmt.Printf("Listening on http://%s\n", addr)
-
-	// 	return http.Serve(l, nil)
-	// })
 	g.Go(func() error {
 		l, err := net.Listen("tcp", os.Getenv("HOST")+":"+os.Getenv("PORT"))
 		if err != nil {
