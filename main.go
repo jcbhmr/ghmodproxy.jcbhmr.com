706fec64cd040642d6b2f699fe3f964f9c69a9cc
Jacob Hummer <jcbhmr@outlook.com>
2026-06-15 06:00:38 +0000

    save

--- main.go
+++ main.go
@@ -3,11 +3,10 @@ package main
 import (
 	"fmt"
 	"log"
-	"math"
 	"net"
 	"net/http"
-
-	"github.com/mdlayher/vsock"
+	"os"
+	"strconv"
 )
 
 func init() {
@@ -17,22 +16,22 @@ func init() {
 }
 
 func main() {
-	// port := uint16(7045)
-	// if v, ok := os.LookupEnv("PORT"); ok {
-	// 	port64, err := strconv.ParseUint(v, 10, 16)
-	// 	if err != nil {
-	// 		log.Fatal(err)
-	// 	}
-	// 	port = uint16(port64)
-	// }
+	port := uint16(8080)
+	if v, ok := os.LookupEnv("PORT"); ok {
+		port64, err := strconv.ParseUint(v, 10, 16)
+		if err != nil {
+			log.Fatal(err)
+		}
+		port = uint16(port64)
+	}
 
-	cid, err := vsock.ContextID()
-	fmt.Printf("cid=%v, err=%v\n", cid, err)
-	var l net.Listener
-	l, err = vsock.ListenContextID(math.MaxUint32, 8080, nil)
+	l, err := net.Listen("tcp", ":"+strconv.FormatUint(uint64(port), 10))
 	if err != nil {
 		log.Fatal(err)
 	}
+	addr := l.Addr().(*net.TCPAddr)
+	fmt.Printf("%d\n", addr.Port)
+
 	err = http.Serve(l, nil)
 	if err != nil {
 		log.Fatal(err)
--- main.ts
+++ main.ts
@@ -1,6 +1,44 @@
 #!/usr/bin/env -S deno --allow-all
-await fetch("https://gloomy-echidna-98.jcbhmr.deno.net/" + Deno.env.get("DENO_DEPLOYMENT_ID"))
+import { TextLineStream } from "@std/streams"
+// await fetch("https://gloomy-echidna-98.jcbhmr.deno.net/" + Deno.env.get("DENO_DEPLOYMENT_ID"))
 
-const child = new Deno.Command(new URL(import.meta.resolve("./app"))).spawn()
-const status = await child.status
-Deno.exit(status.code)
+let port: string | undefined
+async function startChild(): Promise<string> {
+    if (port == null) {
+        const controller = new AbortController()
+        const child = new Deno.Command(new URL(import.meta.resolve("./app")), {
+            args: [],
+            env: {
+                PORT: "0"
+            },
+            stdin: "inherit",
+            stdout: "piped",
+            stderr: "inherit",
+            signal: controller.signal
+        }).spawn()
+        child.unref()
+        globalThis.addEventListener("unload", (_event) => {
+            controller.abort()
+        }, { passive: true })
+        for await (const line of child.stdout.pipeThrough(new TextDecoderStream()).pipeThrough(new TextLineStream())) {
+            port = line;
+            break;
+        }
+        if (port == null) {
+            throw new TypeError("port is null")
+        }
+    }
+    return port
+}
+
+
+export default {
+    async fetch(request) {
+        const requestURL = new URL(request.url)
+        const port = await startChild()
+        const newRequestURL = new URL(requestURL.pathname + requestURL.search, `http://localhost:${port}`)
+        console.log("%s => %s", requestURL, newRequestURL)
+        return await fetch(newRequestURL, request)
+    }
+} satisfies Deno.ServeDefaultExport
+
